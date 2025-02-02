81169596ad880bba16b7c342f817462bf21b1799
Dulanjana Gamage <dulanjana_ngamage@certisgroup.com>
2025-02-02 10:35:47 +0800

    Base Entity Added

---
+++ src/Core/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities {
+
+	/// <summary>
+	/// All common properties goes here, where all entities must have.
+	/// </summary>
+	public abstract class BaseEntity {
+		public Guid CreatedBy { get; set; }
+		public DateTime CreatedOn { get; set; }
+		public Guid ModifiedBy { get; set; }
+		public Guid ModifiedOn { get; set; }
+	}
+}
